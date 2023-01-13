ea461ad63455023ec78effb024b881c3ab8a1065
Shubham Khetre <shubhamek21@gmail.com>
2023-01-13 15:16:54 +0530

    ab ho hona hi chahiye

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,19 +1,19 @@
 package com.driver;
 
 public class Main {
-    public void main(String[] args){
+    public static void main(String[] args){
         B b = new B();
         System.out.println(b.meth());
     }
-    class A{
+    public static class A{
         String meth(){
             return "Invoking method from class A";
         }
     }
 
-    class B extends A {
+    public static class B extends A{
         @Override
-        String meth() {
+        String meth(){
             return "Method is overridden in Extended class B";
         }
     }
